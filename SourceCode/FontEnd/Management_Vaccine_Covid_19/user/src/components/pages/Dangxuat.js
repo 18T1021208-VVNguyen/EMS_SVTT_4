3c000d6869b6b6d0ba2b0a6ab3aa032c51a44614
hoxuannhatminh <89766641+hoxuannhatminh@users.noreply.github.com>
2022-03-23 10:43:18 +0700

    Delete SourceCode/FontEnd/Management_Vaccine_Covid_19 directory

--- SourceCode/FontEnd/Management_Vaccine_Covid_19/user/src/components/pages/Dangxuat.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Dangxuat() {
-  return (
-    <>
-      <h1 className="consulting">Đăng xuất</h1>
-    </>
-  );
-}
--- SourceCode/FontEnd/Management_Vaccine_Covid_19/user/src/components/pages/Hosocanhan.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import { Table } from "react-bootstrap";
-export default function Hosocanhan() {
-  return (
-    <div className="container">
-      <h1>Hồ sơ cá nhân</h1>
-    </div>
-  );
-}

2a2385577c344ad32b689f5162f57ce1556280ca
VVNguyen <87818928+18T1021208-VVNguyen@users.noreply.github.com>
2022-03-24 10:25:26 +0700

    Delete SourceCode/BackEnd/Nguyen directory

--- SourceCode/BackEnd/Nguyen/service/IInjectionHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.laptrinhspringboot.vn.service;
-
-import com.laptrinhspringboot.vn.dto.InjectionHistoryDTO;
-import com.laptrinhspringboot.vn.entity.InjectionHistory;
-
-public interface IInjectionHistoryService {
-	InjectionHistoryDTO getTop1SortInjectionDate(Long customerID);
-}
--- SourceCode/BackEnd/Nguyen/service/IInjectionRegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.laptrinhspringboot.vn.service;
-
-import java.util.Date;
-
-public interface IInjectionRegistrationService {
-	boolean checkRegistrationExpiredGreater( Long idCurtomer,Date today);
-	boolean checkRegistrationExpiredEqual(Long idCurtomer,Date today);
-	
-}
